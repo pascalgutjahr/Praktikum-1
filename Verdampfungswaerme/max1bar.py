ae07c1144ad83c294d1c9c2b3fcf98cce7710d6f
Julian Jung <julian.jung@tu-dortmund.de>
2016-11-14 11:27:55 +0100

    angepasst

--- Verdampfungswaerme/max1bar.py
+++ Verdampfungswaerme/max1bar.py
@@ -4,33 +4,40 @@ from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from scipy.optimize import curve_fit
-plt.rcParams['figure.figsize'] = (15, 10)
+plt.rcParams['figure.figsize'] = (10 , 5)
 plt.rcParams['font.size'] = 13
 plt.rcParams['lines.linewidth'] = 1
 csfont = {'fontname': 'Times New Roman'}
 
 T, p = np.genfromtxt('data1.txt', unpack=True, skip_header=2)
-x = np.log(p)
+#p*=1e+5
+p_0 = 995
+#p_0*= 1e+5
+T-=273.15
+T*=1e-3
+x = np.log(p/p_0)
 
-
-def f(x, m, n):
-    return m * x + n
-
-params, covariance = curve_fit(f, T, x)
+def f(T, m, n):
+    return m * T + n
+#m = -1.58785109712 +- 0.0232138041919
+#n = -9.0289297077 +- 0.118666383813
+params, covariance = curve_fit(f, 1/T, x)
 
 errors = np.sqrt(np.diag(covariance))
 
 print('m =', params[0], '+-', errors[0])
 print('n =', params[1], '+-', errors[1])
 
-x_plot = np.linspace(min(T)-1, max(T)+1)
+x_plot = np.linspace(min(1/T), max(1/T))
 
-plt.plot(T, x, 'rx', label='bis 1bar')
+plt.plot(1/T, x, 'rx', label='Messwerte')
 plt.plot(x_plot, f(x_plot, *params), 'b-', label='linearer Fit')
-# plt.xlim(min(x)-1, max(x)+1)
-plt.xlabel('Temperatur')
-plt.ylabel('log(p)')
+#plt.xlim(min(x)-1, max(x)+1)
+plt.xlabel(r'$T^{-1} \ \cdot 10^{-3}\,\cdot\,\mathrm{K}$')
+plt.ylabel(r'$\ln(p/p_0)$')
+plt.title('Messungen bis 1 bar')
 plt.grid()
+plt.legend()
 plt.tight_layout()
 plt.savefig('fit1.pdf')
 plt.show()
