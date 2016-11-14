c993d95d0c42eae2c26e5710db227e839707d41a
Pascal <pascal.gutjahr@tu-dortmund.de>
2016-11-14 13:33:57 +0100

    15 bar fast fertig

--- Verdampfungswaerme/15bar.py
+++ Verdampfungswaerme/15bar.py
@@ -4,34 +4,37 @@ from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from scipy.optimize import curve_fit
-plt.rcParams['figure.figsize'] = (15, 10)
+plt.rcParams['figure.figsize'] = (12, 8)
 plt.rcParams['font.size'] = 13
 plt.rcParams['lines.linewidth'] = 1
 csfont = {'fontname': 'Times New Roman'}
 
 T, p = np.genfromtxt('data2.txt', unpack=True, skip_header=2)
-T+=273.15
+T += 273.15
 
 
-# def f(p, m, n):
-    # return m * p**2
+def f(T, a, b, c, d):
+    return a * T**3 + b * T**2 + c * T + d
 
-# params, covariance = curve_fit(f, p, T)
+params, covariance = curve_fit(f, T, p)
 
-# errors = np.sqrt(np.diag(covariance))
+errors = np.sqrt(np.diag(covariance))
 
-# print('m =', params[0], '±', errors[0])#
-# print('n =', params[1], '±', errors[1])
+print('a =', params[0], '±', errors[0])
+print('b =', params[1], '±', errors[1])
+print('c =', params[2], '±', errors[2])
+print('d =', params[3], '±', errors[3])
 
 
-# x_plot = np.linspace(min(T), max(T)+0.05)
+x_plot = np.linspace(min(T), max(T))
 
-plt.plot(T, p, 'k-', label='blaah')
-#plt.plot(x_plot, f(x_plot, *params), 'b-', label='linearer Fit', linewidth=2)
+plt.plot(T, p, 'rx', label='blaah')
+plt.plot(x_plot, f(x_plot, *params), 'b-', label='linearer Fit', linewidth=2)
 
 plt.grid()
 plt.title(r'fehlt noch')
 plt.legend(loc='best')
+plt.xlim(375, max(T))
 plt.xlabel(r'$T$')
 plt.tight_layout()
 plt.savefig('15.pdf')
