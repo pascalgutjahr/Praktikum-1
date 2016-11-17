68f2d6f9aa8a3ce8f901bce17df3b9b43f559842
Julian Jung <julian.jung@tu-dortmund.de>
2016-11-17 11:49:37 +0100

    Diskussion beendet

--- Verdampfungswaerme/L+.py
+++ Verdampfungswaerme/L+.py
@@ -4,8 +4,8 @@ from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 # from scipy.optimize import curve_fit
-plt.rcParams['figure.figsize'] = (12, 8)
-plt.rcParams['font.size'] = 13
+plt.rcParams['figure.figsize'] = (15, 10)
+plt.rcParams['font.size'] = 20
 plt.rcParams['lines.linewidth'] = 1
 csfont = {'fontname': 'Times New Roman'}
 
