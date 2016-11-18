e56aa110d9cc53400510c68f491d73084de8572b
Julian Jung <julian.jung@tu-dortmund.de>
2016-11-18 13:39:26 +0100

    Subplot

--- Lock-In/Rausch.py
+++ Lock-In/Rausch.py
@@ -9,11 +9,62 @@ plt.rcParams['font.size'] = 13
 plt.rcParams['lines.linewidth'] = 1
 csfont = {'fontname': 'Times New Roman'}
 
+plt.subplot(2, 1, 1)
+
+phi, U = np.genfromtxt('data1.txt', unpack=True, skip_header=2)
+
+def f(phi,a, b, c, d):
+    return a * phi**3 + b * phi**2 + c * phi + d
+
+params, covariance = curve_fit(f, phi, U)
+
+errors = np.sqrt(np.diag(covariance))
+
+# print('a =', params[0], '±', errors[0])
+# print('b =', params[1], '±', errors[1])
+# print('c =', params[2], '±', errors[2])
+# print('d =', params[3], '±', errors[3])
+#
+
+x_plot = np.linspace(min(phi), max(phi))
+
+
+
+plt.plot(phi, U, 'k.', label='Ohne Rausch')
+plt.plot(x_plot, f(x_plot, *params), 'b-', label='Regression', linewidth=1)
+
+plt.ylabel(r'$U \,/\ 10^{-3} mV $')
+plt.xlabel(r'${\phi}$')
+plt.legend()
+
+plt.grid()
+plt.subplot(2, 1 ,2)
 phi, U = np.genfromtxt('Rausch.txt', unpack=True, skip_header=2)
+
+def f(phi,a, b, c, d):
+    return a * phi**3 + b * phi**2 + c * phi + d
+
+params, covariance = curve_fit(f, phi, U)
+
+errors = np.sqrt(np.diag(covariance))
+
+# print('a =', params[0], '±', errors[0])
+# print('b =', params[1], '±', errors[1])
+# print('c =', params[2], '±', errors[2])
+# print('d =', params[3], '±', errors[3])
+#
+
+x_plot = np.linspace(min(phi), max(phi))
 
 
 plt.plot(phi, U, 'k.', label='Rausch')
-
-plt.grid()
+plt.plot(x_plot, f(x_plot, *params), 'b-', label='Regression', linewidth=1)
 plt.legend()
+plt.grid()
+
+
+
+
+
+
 plt.show()
