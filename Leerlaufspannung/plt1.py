77cf5949821b75bb4b2db549ad73e5c6b58a0022
Julian Jung <julian.jung@tu-dortmund.de>
2016-12-02 12:27:36 +0100

    Plot zu Data1

--- Leerlaufspannung/plt1.py
+++ Leerlaufspannung/plt1.py
@@ -9,7 +9,7 @@ plt.rcParams['font.size'] = 13
 plt.rcParams['lines.linewidth'] = 1
 csfont = {'fontname': 'Times New Roman'}
 
-U, I = np.genfromtxt('data1.txt', unpack=True, skip_header=2)
+R, U, I = np.genfromtxt('data1.txt', unpack=True , skip_header=2)
 
 U /=1000   # in Volt
 I /=1000   # in Ampere
@@ -17,6 +17,25 @@ I /=1000   # in Ampere
 def f(U, m, n):
     return m * U + n
 
-params, covariance = curve_fit(f, U, x)
+params, covariance = curve_fit(f, U, I)
 
 errors = np.sqrt(np.diag(covariance))
+
+print('m =', params[0], '+-', errors[0])
+print('n =', params[1], '+-', errors[1])
+
+# m = -0.0612211228191 +- 0.0010219132743
+# n = 0.0796268668608 +- 0.000932763635657
+
+x_plot = np.linspace(min(U), max(U))
+
+plt.plot(x_plot, f(x_plot, *params), 'b-', label='linearer Fit')
+plt.plot(U,I, 'rx', label='Messwerte')
+plt.xlabel(r'$U \,\mathrm{V}$')
+plt.ylabel(r'$I \,\mathrm{A}$')
+plt.title('Messungen')
+plt.grid()
+plt.legend()
+plt.tight_layout()
+plt.savefig('fit1.pdf')
+plt.show()
