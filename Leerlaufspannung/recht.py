80b3605b0ad9397f5302621c3dde5127c1fc24ca
Julian Jung <julian.jung@tu-dortmund.de>
2016-12-02 12:35:34 +0100

    plot rechteck

---
+++ Leerlaufspannung/recht.py
@@ -0,0 +1,41 @@
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy.optimize import curve_fit
+plt.rcParams['figure.figsize'] = (12, 8)
+plt.rcParams['font.size'] = 13
+plt.rcParams['lines.linewidth'] = 1
+csfont = {'fontname': 'Times New Roman'}
+
+R, U, I = np.genfromtxt('recht.txt', unpack=True , skip_header=2)
+
+U /=1000   # in Volt
+I /=1000   # in Ampere
+
+def f(U, m, n):
+    return m * U + n
+
+params, covariance = curve_fit(f, U, I)
+
+errors = np.sqrt(np.diag(covariance))
+
+print('m =', params[0], '+-', errors[0])
+print('n =', params[1], '+-', errors[1])
+
+# m = -0.0171814649082 +- 0.00014598022858
+# n = 0.00425164331135 +- 2.44196233572e-05
+
+x_plot = np.linspace(min(U), max(U))
+
+plt.plot(x_plot, f(x_plot, *params), 'b-', label='linearer Fit')
+plt.plot(U,I, 'rx', label='Messwerte')
+plt.xlabel(r'$U /\/ \,\mathrm{V}$')
+plt.ylabel(r'$I /\/ \,\mathrm{A}$')
+plt.title('Messungen REchteckspannung')
+plt.grid()
+plt.legend()
+plt.tight_layout()
+plt.savefig('recht.pdf')
+plt.show()
