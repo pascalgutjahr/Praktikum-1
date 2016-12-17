434819b3c335b5e6138837467667da4fa0c02eb9
Julian Jung <julian.jung@tu-dortmund.de>
2016-12-17 18:38:19 +0100

    Programm für Ableitungen

---
+++ Ableitung.py
@@ -0,0 +1,15 @@
+from sympy import *
+import numpy as np
+import scipy as sp
+import math
+
+x = Symbol('x')
+y = Symbol('y')
+z = Symbol('z')
+f = x + y + z
+DelX = f.diff(x)
+DelY = f.diff(y)
+DelZ = f.diff(z)
+print('Ableitung nach x =', DelX)
+print('Ableitung nach y =', DelY)
+print('Ableitung nach z =', DelZ)
