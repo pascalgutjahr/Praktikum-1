e5092e5aa1323ac36985d5c50e23d7737c6c1006
Pascal Gutjahr <pascal.gutjahr@tu-dortmund.de>
2017-01-31 13:09:13 +0100

    bla

---
+++ Drehschwingungen/helm.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+a=[14.247,14.172,14.105,14.085]
+b=[11.123,11.076,11.007,11.124]
+c=[9.653,9.645,9.650,9.644]
+d=[8.622,8.622,8.621,8.612]
+e=[7.873,7.871,7.866,7.846]
+f=[7.191,7.199,7.231,7.227]
+g=[6.902,6.851,8.852,6.849]
+h=[6.504,6.411,6.533,6.549]
+i=[6.184,6.194,6.070,5.997]
+j=[5.803,4.987,5.875,5.789]
+
+print('a=',np.mean(a),'±',np.std(a))
+print('b=',np.mean(b),'±',np.std(b))
+print('c=',np.mean(c),'±',np.std(c))
+print('d=',np.mean(d),'±',np.std(d))
+print('e=',np.mean(e),'±',np.std(e))
+print('f=',np.mean(f),'±',np.std(f))
+print('g=',np.mean(g),'±',np.std(g))
+print('h=',np.mean(h),'±',np.std(h))
+print('i=',np.mean(i),'±',np.std(i))
+print('j=',np.mean(j),'±',np.std(j))
