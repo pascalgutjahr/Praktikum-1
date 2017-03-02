6efd8d05db1115915a80f77d10ea07b928d2a78e
julian.jung@tu-dortmund.de <julian.jung@tu-dortmund.de>
2017-03-02 14:02:04 +0100

    a

--- Ableitung.py
+++ Ableitung.py
@@ -6,10 +6,11 @@ import math
 x = Symbol('x')
 y = Symbol('y')
 z = Symbol('z')
-f = (x**2 * y)/z
+f = y**2*(x**2+y**2)**(0.5)
 DelX = f.diff(x)
 DelY = f.diff(y)
 DelZ = f.diff(z)
 print('Ableitung nach x =', DelX)
 print('Ableitung nach y =', DelY)
 print('Ableitung nach z =', DelZ)
+
