bd0c615c27356ff3d17d5e00fc0bea639682690f
Pascal Gutjahr <pascal.gutjahr@tu-dortmund.de>
2017-04-19 12:20:09 +0200

    ho

--- Roentgen/aurum.py
+++ Roentgen/aurum.py
@@ -12,30 +12,44 @@ mpl.rcParams.update({
     'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}'
 })
 
-t, T1, T2, pa, pb, N = np.genfromtxt('tabelle.txt', unpack=True, skip_header = 3)
+Z, T = np.genfromtxt('txt/plot.txt', unpack=True, skip_header = 3)
+# T = Winkel Theta, Z = Ordnungszahl
 
-x_plot = np.linspace(min(t), max(t))
+The = 2 *np.pi * T / 360 # Winkel in rad
+h = 6.626 *10**(-34)
+c = 299729458
+d = 201 *10**(-12)
+e = 1.602 *10**(-19)
+E_K = (h*c) / (2*d*np.sin(The)) / e
 
+W = np.sqrt(E_K)
+Z2 = Z**2
 
-def f(t, a, b, c):
-    return a*t**2 + b*t + c
+x_plot = np. linspace(90, 130)
 
-params, covariance = curve_fit(f, t, T1)
+def f(W, m, b):
+    return m*W + b
+
+params, covariance = curve_fit(f, W, Z2)
 
 errors = np.sqrt(np.diag(covariance))
 
-print("a=", params[0], "+-", errors[0])
+print("m=", params[0], "+-", errors[0])
 print("b=", params[1], "+-", errors[1])
-print("c=", params[2], "+-", errors[2])
 
-plt.plot(x_plot, f(x_plot, *params), 'r-', label='Regression T$_1$(t)', linewidth=1)
+# m= 22.9616004655 +- 3.5769453201
+# b= -1348.56433773 +- 404.895551595
+
+plt.plot(x_plot, f(x_plot, *params), 'r-', label='lineare Regression', linewidth=1)
 
 
-plt.plot(t, T1, 'rx', label='Temperatur des warmen Reservoirs')
-plt.xlabel(r'$t\,/\,\si{\second}')
-plt.ylabel(r'$T\,/\,\si{\kelvin}')
-plt.xlim(min(t), max(t))
+plt.plot(W, Z2, 'rx', label='Messwerte')
+plt.plot()
+plt.ylabel(r'$Z^2')
+plt.xlabel(r'$\sqrt{E_K}\,/\,\si{\electronvolt}')
+plt.xlim(min(W)-2, max(W)+2)
+plt.ylim(850, 1650)
 plt.grid()
 plt.legend(loc='best')
 plt.tight_layout()
-plt.savefig('bilder/aurum.pdf')
+plt.savefig('bilder/plot.pdf')
