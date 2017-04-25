55291b454625a0ac38e681c7a15f8afb2274d402
Pascal Gutjahr <pascal.gutjahr@tu-dortmund.de>
2017-04-25 18:04:59 +0200

    mittel geändert

--- Suszeptibilitaet/mittel.py
+++ Suszeptibilitaet/mittel.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-x = np.array([84, 97, 123, 138, 155])
-x * 5
-print('Mittelwert:', np.mean(x))
-print('Std:', np.std(x))
+x = [84, 97, 123, 138, 155]
+P =  5
+xp = P * np.array(x)
+print('Mittelwert:', np.mean(xp))
+print('Std:', np.std(xp))
 
 x = [84, 97, 123, 138, 155]
 x = x * 5
