6591f3ebe5766619818713470c615416ba7c5a90
Pascal Gutjahr <pascal.gutjahr@tu-dortmund.de>
2017-04-25 22:30:36 +0200

    mittel

--- Suszeptibilitaet/mittel.py
+++ Suszeptibilitaet/mittel.py
@@ -6,7 +6,15 @@ xp = P * np.array(x)
 print('Mittelwert:', np.mean(xp))
 print('Std:', np.std(xp))
 
-x = [84, 97, 123, 138, 155]
-x = x * 5
-print('Mittelwert:', np.mean(x))
-print('Std:', np.std(x))
+y =np.array( [162,154, 146, 117,206]) 
+yp = P * np.array(y)
+print('Mittelwert:', np.mean(yp))
+print('Std:', np.std(yp))
+
+
+z =np.array( [164, 158, 172, 184, 227])
+zp = P * np.array(z)
+a =zp-yp 
+print('Differenz zy:',a) 
+print('Mittelwert der Differenz:', np.mean(a))
+print('Std der Differenz:', np.std(a))
