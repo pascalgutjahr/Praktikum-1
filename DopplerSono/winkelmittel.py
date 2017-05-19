17e7e14dc9b1f26478e13da1d7e43bf62df7e276
Pascal Gutjahr <pascal.gutjahr@tu-dortmund.de>
2017-05-19 11:47:59 +0200

    fertig

--- DopplerSono/winkelmittel.py
+++ DopplerSono/winkelmittel.py
@@ -19,12 +19,12 @@ nu15 = nu15 / np.cos(1.397)
 nu30 = nu30 / np.cos(1.231)
 nu60 = nu60 / np.cos(0.955)
 
-plt.plot(v15, nu15, 'bx', label='$\theta= 15\,\si{\degree}$ Winkel')
-plt.plot(v30, nu30, 'rx', label='$\theta= 30\,\si{\degree}$ Winkel')
-plt.plot(v60, nu60, 'gx', label='$\theta= 60\,\si{\degree}$ Winkel')
+plt.plot(v15, nu15/1000, 'bx', label=r'$\theta= 15\,\si{\degree}$ Winkel')
+plt.plot(v30, nu30/1000, 'rx', label=r'$\theta= 30\,\si{\degree}$ Winkel')
+plt.plot(v60, nu60/1000, 'gx', label=r'$\theta= 60\,\si{\degree}$ Winkel')
 plt.grid()
 plt.xlabel(r'$v \,/\,\si[per-mode=fraction]{\meter\per\second}$')
-plt.ylabel(r'$\frac{\Delta\nu}{\cos{\alpha}}\,/\,\si{\hertz}$')
+plt.ylabel(r'$\frac{\Delta\nu}{\cos{\alpha}}\,/\,\si{\kilo\hertz}$')
 plt.legend(loc='best')
 
 plt.tight_layout
