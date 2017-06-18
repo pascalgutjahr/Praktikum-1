c5eff0573ecc6087dcbdb1c7006364c83409c835
Julian Jung <julian.jung@tu-dortmund.de>
2017-06-18 13:35:28 +0200

    test

--- Beugung/doppel1.py
+++ Beugung/doppel1.py
@@ -18,15 +18,30 @@ z_2 = z_2 * 1e-3
 z_0 = 28.90e-3
 z = z_2 - z_0
 I_2 = I_2 * 1e-9 - 0.3e-9
-L = 1
+l = 1
 A_0 = 970e-9
 lmbd = 532e-9
 b = 0.1e-3
 s = 0.4e-3
 
+def f(z_2,a,b,l):
+    return 4*(np.cos((np.pi*a*np.sin((z_2 - 0.041) / 0.885))/l))**2 * (l/(np.pi*b*np.sin
+    ((z_2 - 0.041) / 0.885)))**2 * (np.sin((np.pi*b*np.sin((z_2 - 0.041) / 0.885))/l))**2
+
+params, covariance= curve_fit(f, z_2, I_2)
+errors = np.sqrt(np.diag(covariance))
+
+print('a =', params[0], '±', errors[0])
+print('b =', params[1], '±', errors[1])
+print('l =', params[2], '±', errors[2])
+# print("T=",T)
+
+
+
 B = np.sqrt(4 * np.cos(np.pi*s*np.sin(z)/lmbd)**2 * (lmbd/np.pi*b*np.sin(z))**2 * np.sin(np.pi*b*np.sin(z)/lmbd)**2)
 b = 4 * A_0 * np.cos(z/2)**2
 plt.plot(z/1e-3, B/1e-9, 'r-', label='Theoriekurve')
+plt.plot((z_2-0.041),f(z_2, *params), '-', color = 'blueviolet', label='Ausgleichsgerade', linewidth=1)
 plt.plot(z/1e-3, I_2/1e-9, 'bx', label='gemessene Intensität')
 plt.grid()
 plt.xlabel(r'$\zeta - \zeta_0\,/\,\si{\milli\meter}$')
