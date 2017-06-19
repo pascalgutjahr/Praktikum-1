55d419783346d678faa061f3a6089ce7c4d34d1f
julian.jung@tu-dortmund.de <julian.jung@tu-dortmund.de>
2017-06-19 12:59:27 +0200

    .

--- Beugung/doppel1.py
+++ Beugung/doppel1.py
@@ -6,48 +6,51 @@ plt.rcParams['lines.linewidth'] = 1
 import numpy as np
 
 mpl.rcParams.update({
-   'font.family': 'serif',
-   'text.usetex': True,
-   'pgf.rcfonts': False,
-   'pgf.texsystem': 'lualatex',
-   'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}'
+'font.family': 'serif',
+'text.usetex': True,
+'pgf.rcfonts': False,
+'pgf.texsystem': 'lualatex',
+'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}'
 })
 
-z_1, I_1, z_2, I_2, z_3, I_3 = np.genfromtxt('werte.txt', unpack=True, skip_header=2)
-z_2 = z_2 * 1e-3
-z_0 = 28.90e-3
-z = z_2 - z_0
-I_2 = I_2 * 1e-9 - 0.3e-9
+x = np.array([22.65,22.90,23.15,23.40,23.65,23.90,24.15,24.40,24.65,24.90,25.15,25.40,
+25.65,25.90,26.15,26.40,26.65,26.90,27.15,27.40,27.65,27.90,28.15,28.40,28.65,28.90,29.15,
+29.40,29.65,29.90,30.15,30.40,30.65,30.90,31.15,31.40,31.65,31.90,32.15,32.40,32.65,32.90,
+33.15,33.40,33.65,33.90,34.15,34.40,34.65,34.90,35.15])
+
+I = np.array([32.0, 14.0,  4.4,  5.0,  3.6,  1.8,  2.0,  8.0, 30.0, 40.0,  2.0,  4.0,160.0,
+210.0,100.0,100.0,380.0,540.0,270.0,140.0,550.0,860.0,530.0,200.0,780.0,970.0,740.0,240.0,420.0,
+840.0,720.0,250.0,250.0,540.0,520.0,210.0,120.0,220.0,240.0,120.0, 50.0, 64.0, 65.0, 41.0, 22.0, 10.0,
+  6.0, 10.0,  9.0,  9.0, 10.0])
+
+I = (I-0.3)*10**(-6)
+x = x*1e-3
+a = 970e-9
+b = 0.015e-3
 l = 1
-A_0 = 970e-9
-lmbd = 532e-9
-b = 0.1e-3
-s = 0.4e-3
+lam = 532e-9
+phi = (x - 0.0289) / l
 
-def f(z_2,a,b,l):
-    return 4*(np.cos((np.pi*a*np.sin((z_2 - 0.041) / 0.885))/l))**2 * (l/(np.pi*b*np.sin
-    ((z_2 - 0.041) / 0.885)))**2 * (np.sin((np.pi*b*np.sin((z_2 - 0.041) / 0.885))/l))**2
+j = a * ((np.sin((np.pi * b * np.sin(phi))/lam)) / ((np.pi * b * np.sin(phi)) / (lam)))**2
 
-params, covariance= curve_fit(f, z_2, I_2)
+
+def f(x,a,b,l):
+	return 4*(np.cos((np.pi*a*np.sin((x - 0.0289) / 1))/lam))**2 * (lam/(np.pi*b*np.sin((x - 0.0289) / 1)))**2 * (np.sin((np.pi*b*np.sin((x - 0.0289) / 1))/lam))**2
+	#a=s,b=b,l=lam
+params, covariance= curve_fit(f, x, I)
 errors = np.sqrt(np.diag(covariance))
 
 print('a =', params[0], '±', errors[0])
 print('b =', params[1], '±', errors[1])
 print('l =', params[2], '±', errors[2])
-# print("T=",T)
 
+plt.plot((x-0.02835),f(x,*params), '-', color = 'blueviolet', label='Ausgleichsgerade', linewidth=1)
+plt.plot((x-0.02835), j*10**6, '-', color = "dodgerblue", label = "Ausgleichsgerade", linewidth = 1)
+plt.plot((x-0.02835), I*10**6, '.', color = "indianred", label = "Messwerte", linewidth = 1)
 
-
-B = np.sqrt(4 * np.cos(np.pi*s*np.sin(z)/lmbd)**2 * (lmbd/np.pi*b*np.sin(z))**2 * np.sin(np.pi*b*np.sin(z)/lmbd)**2)
-b = 4 * A_0 * np.cos(z/2)**2
-plt.plot(z/1e-3, B/1e-9, 'r-', label='Theoriekurve')
-plt.plot((z_2-0.041),f(z_2, *params), '-', color = 'blueviolet', label='Ausgleichsgerade', linewidth=1)
-plt.plot(z/1e-3, I_2/1e-9, 'bx', label='gemessene Intensität')
+plt.xlabel(r'$\zeta - \zeta_0 \,/\,\si{\meter}$')
+plt.ylabel(r'$I\,/\,\si{\micro\ampere}$')
 plt.grid()
-plt.xlabel(r'$\zeta - \zeta_0\,/\,\si{\milli\meter}$')
-plt.ylabel(r'$I\,/\,\si{\nano\ampere}$')
-plt.legend(loc='best')
-
-plt.tight_layout
-plt.savefig('bilder/doppel1.pdf')
-plt.show()
+plt.legend()
+plt.tight_layout()
+plt.savefig("bilder/Einzelspalt.pdf")
