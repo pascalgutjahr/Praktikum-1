2a3ebf7abc0974cf55495ca233e9dba36e56b516
Pascal Gutjahr <pascal.gutjahr@tu-dortmund.de>
2017-06-26 13:49:28 +0200

    fertig

--- GeometOptik/abbeG.py
+++ GeometOptik/abbeG.py
@@ -16,12 +16,15 @@ mpl.rcParams.update({
 # V1 = 1 + 1/V
 # V2 = 1 + V
 g = np.array([ 17.6,  16.0,   18.4,  19.5,  21.2,  25.1,  17.3,  17.3,  18.7,  21.0 ])*10**(-2)
-V1 = np.array([ 4.76,  4.6,   4.84,  4.95,  5.12,  5.51,  4.73,  4.73,  4.87,  5.1 ])
-b = np.array([ 87.4,  84.0,   76.6,  70.5,  63.8,  54.9,  92.7,  85.2,  73.8,  66.5])
-V2 = np.array([ 11.74,  11.4,   10.66,  10.05,   9.38,   8.49,  12.27,  11.52,  10.38,   9.65])
+# V1 = np.array([ 4.76,  4.6,   4.84,  4.95,  5.12,  5.51,  4.73,  4.73,  4.87,  5.1 ])
+# b = np.array([ 87.4,  84.0,   76.6,  70.5,  63.8,  54.9,  92.7,  85.2,  73.8,  66.5])
+# V2 = np.array([ 11.74,  11.4,   10.66,  10.05,   9.38,   8.49,  12.27,  11.52,  10.38,   9.65])
+
+V1 = np.array([ 4.97,  5.25,  4.16,  3.62,  3.01,  2.19,  5.36,  4.92,  3.95,  3.17])
+
 
 def f(V1, m, n):
-   return m * V1 + n
+   return m * (1+ 1/V1) + n
 
 params, covariance = curve_fit(f, V1, g)
 
@@ -30,10 +33,10 @@ errors = np.sqrt(np.diag(covariance))
 print('m =', params[0], '+-', errors[0])
 print('n =', params[1], '+-', errors[1])
 
-plt.plot(V1, f(V1, *params), 'k-', label='linearer Fit: g')
+plt.plot(1+1/V1, f(V1, *params), 'k-', label='linearer Fit: g')
 
 
-plt.plot(V1, g, '.', color='green')
+plt.plot(1+1/V1, g, '.', color='green')
 plt.xlabel(r'$1 + \frac{1}{V}$')
 plt.ylabel(r'$g´\,/\,\si{\meter}$')
 plt.tight_layout()
