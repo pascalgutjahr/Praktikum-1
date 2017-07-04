06d8bf7789a627d007616f5afafa42f0852396b6
julian.jung@tu-dortmund.de <julian.jung@tu-dortmund.de>
2017-07-04 13:42:42 +0200

    kennlinien

--- Elektronenemission/plotA.py
+++ Elektronenemission/plotA.py
@@ -13,6 +13,15 @@ mpl.rcParams.update({
 'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}'
 })
 
-U, I = np.genfromtxt('teilA.txt', skip_header = 1, unpack=True)
+U, I1, I2, I3, I4, I5 = np.genfromtxt('teilA.txt', skip_header = 1, unpack=True)
 
-plt.plot(U,I, 'x', color = 'red', linewidth=1)
+plt.plot(U, I1, 'x', color = 'red', label = r'$I_\symup{f}=1,8\si{\ampere}$')
+plt.plot(U, I2, 'x', color = 'blue', label = r'$I_\symup{f}=1,9\si{\ampere}$')
+plt.plot(U, I3, 'x', color = 'green', label = r'$I_\symup{f}=2,0\si{\ampere}$')
+plt.plot(U, I4, 'x', color = 'purple', label= r'$I_\symup{f}=2,2\si{\ampere}$')
+plt.plot(U, I5, 'x', color = 'black', label= r'$I_\symup{f}=2,4\si{\ampere}$')
+plt.xlabel(r'$U\,/\,\si{\volt}$')
+plt.ylabel(r'$I\,/\,\si{\nano\ampere}$')
+plt.grid()
+plt.legend(loc='best')
+plt.savefig('bilder/kennlinie.pdf')
